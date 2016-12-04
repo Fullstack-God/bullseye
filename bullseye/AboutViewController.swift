f8c286a4a322e375fad24fb4dcc73f2266920d5b
Tim Myers <timmyers1978@gmail.com>
2016-12-04 01:55:59 -0700

    almost finished

---
+++ bullseye/AboutViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AboutViewController.swift
+//  bullseye
+//
+//  Created by Timothy Myers on 12/4/16.
+//  Copyright © 2016 Rocky Mountain Apps. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html") {
+            if let htmlData = try? Data(contentsOf: url) {
+                let baseURL = URL(fileURLWithPath: Bundle.main.bundlePath)
+                webView.load(htmlData, mimeType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+            }
+        }
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
--- bullseye/ViewController.swift
+++ bullseye/ViewController.swift
@@ -9,26 +9,111 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let thumbImageNormal = #imageLiteral(resourceName: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+        
+        let thumbImageHighlighted = #imageLiteral(resourceName: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        let trackLeftImage = #imageLiteral(resourceName: "SliderTrackLeft")
+        let trackLeftResizable = trackLeftImage.resizableImage(withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        
+        let trackRightImage = #imageLiteral(resourceName: "SliderTrackRight")
+        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
+        
+        startNewGame()
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
 
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello, World!",
-                                      message: "This is my first app, NOT!",
+        
+        let difference = abs(targetValue - currentValue)
+        var points = 100 - difference
+        
+        //score += points
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        } else if difference == 1 {
+            title = "So close!"
+            points += 50
+        } else if difference < 5{
+            title = "Almost had it!"
+        } else {
+            title = "Not even close..."
+        }
+        score += points
+        
+        let message = "You scored \(points) points"
+        
+        let alert = UIAlertController(title: title,
+                                      message: message,
                                       preferredStyle: .alert)
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let action = UIAlertAction(title: "OK", style: .default,
+            handler: {
+                action in
+                    self.startNewRound()
+                    self.updateLabels()
+                })
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+
+    }
+    
+    @IBAction func startOver() {
+        startNewGame()
+        updateLabels()
+    }
+    
+    func startNewRound() {
+        round += 1
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
+    func startNewGame() {
+        score = 0
+        round = 0
+        startNewRound()
+        updateLabels()
     }
 }
 
